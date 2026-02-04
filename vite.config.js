166cc0cbec699512ad24239d1686b190a299f2e3
Adrian Seth Tabotabo <adriangwapoz125@gmail.com>
2026-02-04 23:48:03 +0800

    Remove CRA setup and dependencies, clean up for Vite migration

---
+++ vite.config.js
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  
+  server: {
+    port: 5173,
+    open: true,
+  },
+  
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    
+    sourcemap: false,
+    
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('/components/')) {
+            return 'components';
+          }
+          if (id.includes('/services/')) {
+            return 'services';
+          }
+          if (id.includes('/utils/')) {
+            return 'utils';
+          }
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+    
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096,
+    chunkSizeWarningLimit: 1000,
+  },
+})
